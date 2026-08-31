167634854e82c2ac7d617dfbcf090a43f1c6f25f
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-08-31 14:46:02 -0500

    Actualizar yts.js

--- modulos/download/yts.js
+++ modulos/download/yts.js
@@ -7,6 +7,7 @@ module.exports = {
   name: 'yts',
   aliases: ['ytsearch'],
   description: 'Busca videos en YouTube',
+  category: 'download',
 
   async execute(sock, msg, args) {
     const jid = msg.key.remoteJid
