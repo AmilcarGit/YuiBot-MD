6c378eb39fb36477b7107ed7b45e18b7c7bb5e87
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-08-31 12:45:20 -0500

    Actualizar defaults.js

--- defaults.js
+++ defaults.js
@@ -1,6 +1,8 @@
 //CÓDIGO ORIGINAL DE YUIBOT-MD
 module.exports = {
   BOT_NAME: 'YuiBot-MD',
-  PREFIX: '.', // cambia esto si quieres otro prefijo, ej: "/" o "."
+  PREFIXES: ['!', '.', '/', '#'],
+  ALLOW_NO_PREFIX: true,
+  OWNERS: ['51910227479'],
   BOT_VERSION: '1.0.0',
 };
