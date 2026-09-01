b3302a350534bb99a4a42ebd3d80b89721f856a4
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-09-01 06:12:07 -0500

    Actualizar defaults.js

--- defaults.js
+++ defaults.js
@@ -16,6 +16,7 @@ module.exports = {
     LEMPI_KEY: 'lem_dc158e5ad3f4f6ee2de2905a222bfb68f61dd754',
     EVOGB_KEY: 'evogb-jRhjmDSp',
     MITZUKI_KEY: 'sk-c8498d1dfbff805b5c10823a491082714dd76ac6f9a9e03dfe12ffc9b646d9a4',
+    DVYER_KEY: 'dvyer069408482476',
   },
   MENU_IMAGES: [
     { ruta: path.join(__dirname, 'media', 'menu1.mp4'), animado: true },
