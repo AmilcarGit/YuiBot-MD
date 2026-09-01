f4e9c2d273c00b7463db609255150e4ad3f38998
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-09-01 06:42:45 -0500

    Actualizar defaults.js

--- defaults.js
+++ defaults.js
@@ -13,7 +13,7 @@ module.exports = {
   PHONE_NUMBER: '',
   PM2_PROCESS_NAME: 'YuiBot-MD',
   WELCOME_ENABLED: true,
-  WELCOME_BACKGROUND: 'https://api.lempi.lat/tSQ.jpg',
+  WELCOME_BACKGROUND: 'https://files.catbox.moe/vajp5d.jpeg',
   APIS: {
     LEMPI_KEY: 'lem_dc158e5ad3f4f6ee2de2905a222bfb68f61dd754',
     EVOGB_KEY: 'evogb-jRhjmDSp',
