32edb97ecbf7623b9a356ce62b77c9626fd17dfa
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-09-01 23:03:36 -0500

    Actualizar defaults.js

--- defaults.js
+++ defaults.js
@@ -21,6 +21,14 @@ module.exports = {
     MIN: 5,
     MAX: 15,
   },
+  ECONOMIA: {
+    DAILY: {
+      COOLDOWN_MS: 86400000,
+      MIN: 100,
+      MAX: 300,
+    },
+    TRIVIA_RECOMPENSA: 50,
+  },
   APIS: {
     LEMPI_KEY: 'lem_dc158e5ad3f4f6ee2de2905a222bfb68f61dd754',
     EVOGB_KEY: 'evogb-jRhjmDSp',
