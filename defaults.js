bda5f03770a7c2b090b550a6001a27403df217d8
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-09-02 06:02:29 -0500

    Actualizar defaults.js

--- defaults.js
+++ defaults.js
@@ -29,6 +29,14 @@ module.exports = {
     },
     TRIVIA_RECOMPENSA: 50,
   },
+  MODERACION: {
+    ANTILINK: true,
+    ANTIFLOOD: {
+      ENABLED: true,
+      MAX_MENSAJES: 5,
+      VENTANA_MS: 7000,
+    },
+  },
   APIS: {
     LEMPI_KEY: 'lem_dc158e5ad3f4f6ee2de2905a222bfb68f61dd754',
     EVOGB_KEY: 'evogb-jRhjmDSp',
