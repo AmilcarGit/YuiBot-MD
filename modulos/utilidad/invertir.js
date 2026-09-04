428c643faba49fd1676d2aa2cce4097f3baedbe5
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-09-04 11:52:53 -0500

    Crear invertir.js

---
+++ modulos/utilidad/invertir.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: 'invertir',
+  alias: ['reverse'],
+  description: 'Invierte el texto',
+
+  async execute(socket, msg, args) {
+    const texto = args.join(' ').trim()
+
+    if (!texto) {
+      return await msg.reply(
+        '✳️ *USO DEL COMANDO*\n\n' +
+        'Invierte el orden de los caracteres.\n\n' +
+        '📌 Ejemplo:\n' +
+        '`.invertir hola mundo`'
+      )
+    }
+
+    const resultado = [...texto].reverse().join(' ')
+
+    await msg.reply(`🔄 *TEXTO INVERTIDO*\n\n${resultado}`)
+  }
+}
