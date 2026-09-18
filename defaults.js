e01e99c3cc520f77a9b83124135dc6a83ca98ece
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-09-18 11:10:05 -0500

    Actualizar defaults.js

--- defaults.js
+++ defaults.js
@@ -65,6 +65,7 @@ module.exports = {
     MITZUKI_KEY: 'sk-c8498d1dfbff805b5c10823a491082714dd76ac6f9a9e03dfe12ffc9b646d9a4',
     DVYER_KEY: 'dvyer069408482476',
     OCR_KEY: 'K83794019188957',
+    ORBIT_KEY: 'ORBIT-4096939993',
   },
   MENU_IMAGES: [
     { ruta: path.join(__dirname, 'media', 'menu1.mp4'), animado: true },
